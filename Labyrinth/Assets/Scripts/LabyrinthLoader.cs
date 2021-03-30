e88aa5af46481273df0dccd0e969bfd000d1391b
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2021-03-30 15:57:27 +0200

    Filled grid reimplemented

---
+++ Labyrinth/Assets/Scripts/LabyrinthLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthLoader : MonoBehaviour
+{
+    public int mazeRows, mazeCols;
+    public GameObject cell;
+    public float size = 2f;
+    public float gridSpacingOffset = 1f;
+
+    private MazeCell[,] cells;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeMaze();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeMaze()
+    {
+        cells = new MazeCell[mazeRows, mazeCols];
+        for(int r = 0; r < mazeRows; r++)
+        {
+            for (int c = 0; c < mazeCols; c++)
+            {
+                cells[r, c] = new MazeCell();
+                Vector3 spawnPosition = new Vector3(r * gridSpacingOffset, 0, c * gridSpacingOffset) + Vector3.zero;
+                cells[r, c].body = Instantiate(cell, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Labyrinth/Assets/Scripts/MazeCell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell
+{
+    public bool visited = false;
+    public bool isWall = true;
+    public MazeCell[] neighbors = null;
+    public GameObject body;
+}
