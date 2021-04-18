b3785c6588b0bf6bd86cc6720178dae52d99e5c7
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2021-04-18 22:34:49 +0200

    Enemy works with timer, if detects Player / Prefabs

--- Labyrinth/Assets/Scripts/Laser.cs
+++ Labyrinth/Assets/Scripts/Laser.cs
@@ -7,6 +7,8 @@ public class Laser : MonoBehaviour
     private LineRenderer lr;
     public GameObject Enemy;
     public bool Enemyfound = false;
+    public float timeValue = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +27,27 @@ public class Laser : MonoBehaviour
             {
                 if(hit.collider.tag == "Player")
                 {
-                    //    Debug.LogError(hit.collider.tag);
                     Enemyfound = true;
+                    timeValue = 10;
                 }
                 
                 lr.SetPosition(1, hit.point);
             }
         }
         else lr.SetPosition(1, Enemy.transform.forward*50000);
+        Timer();
+    }
+
+    void Timer()
+    {
+        if(timeValue  > 0 && Enemyfound)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 10;
+            Enemyfound = false;
+        }
     }
 }
