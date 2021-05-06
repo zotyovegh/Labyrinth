7a75f232529028407e5ee9daf1ed4443eab86693
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2021-05-06 22:36:59 +0200

    Value update on slider

---
+++ Labyrinth/Assets/Scripts/SliderDisplayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderDisplayer : MonoBehaviour
+{
+    TMPro.TextMeshProUGUI textToUpdate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textToUpdate = GetComponent<TMPro.TextMeshProUGUI>();
+        
+    }
+
+    public void valueUpdate(float value)
+    {
+        textToUpdate.text = Mathf.RoundToInt(value) + "";
+    }
+}
