0482b083474dda72cb5a6728e505b6f42454906c
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2021-05-11 23:55:39 +0200

    Finished screen #3 Buttons work

---
+++ Labyrinth/Assets/Scripts/Menu/FinishedMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishedMenuController : MonoBehaviour
+{
+    public void Restart()
+    {
+        GameSetup.isFinished = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Menu()
+    {
+        GameSetup.isFinished = false;
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
