93f2c850da193b2c1d0a1744808063757034847a
Zoltan Vegh <zotyovegh@gmail.com>
2021-05-12 22:42:29 +0200

    Hammer animation on swing

--- Labyrinth/Assets/Scripts/MouseActions/HammerController.cs
+++ Labyrinth/Assets/Scripts/MouseActions/HammerController.cs
@@ -8,6 +8,13 @@ public class HammerController : MonoBehaviour
     public string selectableTag = "Selectable";
     public float clickType = -1;
     public int destroyDistance = 10;
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
 
     void Update()
     {
@@ -49,6 +56,9 @@ public class HammerController : MonoBehaviour
             if (wallScript.ObjectWest) DestroyTorchOnWall(wallScript.ObjectWest, wallScript);
 
             wallScript.DestroyWall();
+            //anim
+            animator.SetTrigger("isSwinging");
+
             GameSetup.hammerLife--;
             if (GameSetup.hammerLife == 0) {
                 Destroy(gameObject);                    
